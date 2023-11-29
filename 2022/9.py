77fadce704daa9b998dc07059268dc0148c1c491
Eduardo Arnold <4097245+eduardohenriquearnold@users.noreply.github.com>
2023-11-29 11:47:54 -0500

    Day 9 final solution

--- 2022/9.py
+++ 2022/9.py
@@ -1,24 +1,47 @@
+from math import sqrt
+
+
+def clip(c: complex) -> complex:
+    """Returns complex where real and imag. parts are the signs of original complex."""
+    real = c.real / abs(c.real) if c.real != 0 else 0
+    imag = c.imag / abs(c.imag) if c.imag != 0 else 0
+    return complex(real, imag)
+
+
 class Rope:
-    def __init__(self) -> None:
-        self.head = [0, 0]
-        self.tail = [0, 0]
+    def __init__(self, length: int) -> None:
+        self.knots = [complex()] * length
+        self.tail_history = set()
 
     def move_head(self, direction: str, quantity: int):
-        axis_directions = {'U':(1, 1), 'D':(1, -1), 'R':(0, 1), 'L':(0, -1)}
+        axis_directions = {"U": 1j, "D": -1j, "R": 1, "L": -1}
         assert direction in axis_directions.keys(), "invalid direction"
-        axis, amount = axis_directions[direction]
-        
+
         for _ in range(quantity):
-            self.head[axis] += amount
-            self.update_tail()
+            self.knots[0] += axis_directions[direction]
+            self.update_knots()
 
-    def update_tail(self):
-        diff  = [h-t for h, t in zip(self.head, self.tail)]
+    def update_knots(self):
+        for i in range(1, len(self.knots)):
+            diff = self.knots[i - 1] - self.knots[i]
 
-        # head is two steps up/down/left/right
-        if (diff[0] == 0 or diff[1] == 0) and (abs(diff[0])>1 or abs(diff[1])>1):
-            direction = [d/max((abs(x) for x in diff)) for d in diff]
-            self.tail = [t + dir for t, dir in zip(self.tail, direction)]
-        #TODO: other cases
+            if abs(diff) > sqrt(2):
+                self.knots[i] += clip(diff)
 
-        
+        self.tail_history.add(self.knots[-1])
+
+    def tail_history_length(self) -> int:
+        return len(self.tail_history)
+
+
+rope2 = Rope(length=2)
+rope10 = Rope(length=10)
+for line in open("inputs/9.txt", "r").readlines():
+    direction, step = line.strip().split(" ")
+    step = int(step)
+
+    rope2.move_head(direction=direction, quantity=int(step))
+    rope10.move_head(direction=direction, quantity=int(step))
+
+print(rope2.tail_history_length())
+print(rope10.tail_history_length())
