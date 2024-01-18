14b0717cbb60f54faa757801822c77a9ecda3a20
Eduardo Arnold <4097245+eduardohenriquearnold@users.noreply.github.com>
2024-01-18 21:29:36 +0000

    2023: attempt to get Part2 of day 24. Didn't work, now I gave up.

--- 2023/24.cpp
+++ 2023/24.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <cassert>
+
+// Requires installing libeigen3-dev
+// and compiling with flag -I/usr/include/eigen3
+#include <Eigen/Core>
+#include <Eigen/Dense>
 
 using intt = long;
 
@@ -60,8 +64,47 @@ int count_intersections(const std::vector<Line> &lines)
         for (auto it1 = std::next(it0); it1 != lines.end(); it1++)
             if (it0->intersect(*it1))
                 count++;
-
     return count;
+}
+
+Eigen::Matrix3d cross_matrix(Eigen::Vector3d v)
+{
+    Eigen::Matrix3d result;
+    result << 0, -v[2], v[1],
+        v[2], 0, -v[0],
+        -v[1], v[0], 0;
+    return result;
+}
+
+// had to resort to Eigen and online wisdom
+intt armageddon_rock(const std::vector<Line> &lines)
+{
+    // let p0, v0 be the R^3 position and velocity of the rock, and pi, vi the equivalent for i-th hailstone
+    // then p0 + ti * v0 = pi + ti * vi
+    // =>  p0 - pi = ti * (vi - v0), ti \in Z+
+    // =>  (p0 - pi) is parallel to (vi - v0) ==> (p0 - pi) x (vi - v0) = 0
+    // replacing the non-linear p0*v0 terms for different i's
+    // we get p0 x (v1 - v2) + v0 x (p1 - p2) = p1 x v1 - p2 x v2
+    // this above equation gives 3 lines in the system Ax = b, where x = [x, y, z, vx, vy, vz] of the stone
+    Eigen::MatrixXd A(6, 6);
+    Eigen::VectorXd b(6);
+    auto Vec3p = [](const Line &l)
+    { return Eigen::Vector3d(l.x, l.y, l.z); };
+    auto Vec3v = [](const Line &l)
+    { return Eigen::Vector3d(l.vx, l.vy, l.vz); };
+
+    b.segment<3>(0) = -Vec3p(lines[0]).cross(Vec3v(lines[0])) + Vec3p(lines[1]).cross(Vec3v(lines[1]));
+    b.segment<3>(3) = -Vec3p(lines[0]).cross(Vec3v(lines[0])) + Vec3p(lines[2]).cross(Vec3v(lines[2]));
+    A.block<3, 3>(0, 0) = cross_matrix(Vec3v(lines[0])) - cross_matrix(Vec3v(lines[1]));
+    A.block<3, 3>(3, 0) = cross_matrix(Vec3v(lines[0])) - cross_matrix(Vec3v(lines[2]));
+    A.block<3, 3>(0, 3) = -cross_matrix(Vec3p(lines[0])) + cross_matrix(Vec3p(lines[1]));
+    A.block<3, 3>(3, 3) = -cross_matrix(Vec3p(lines[0])) + cross_matrix(Vec3p(lines[2]));
+    Eigen::VectorXd result = A.inverse() * b;
+    std::cout << result << std::endl;
+    intt sum = 0;
+    for (int i = 0; i < 3; i++)
+        sum += intt(result[i]);
+    return sum;
 }
 
 int main()
@@ -72,5 +115,6 @@ int main()
         lines.push_back(Line(is));
 
     std::cout << count_intersections(lines) << std::endl;
+    std::cout << armageddon_rock(lines) << std::endl;
     return 0;
 }
